2142462e8913f5e5c6738f59447848f79d1edbc9
Nataly1087 <nnata100887@gmail.com>
2024-12-07 17:17:16 +0300

    add translator and resume to menu, create new messages

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes
+from telegram.ext import Application, CallbackQueryHandler, ContextTypes, MessageHandler, filters, CommandHandler
 
+from credentials import*
 from gpt import ChatGptService
-from util import (load_message, send_text, send_image, show_main_menu,
-                  default_callback_handler)
+from util import*
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -15,20 +15,73 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'random': 'Узнать случайный интересный факт 🧠',
         'gpt': 'Задать вопрос чату GPT 🤖',
         'talk': 'Поговорить с известной личностью 👤',
-        'quiz': 'Поучаствовать в квизе ❓'
-        # Добавить команду в меню можно так:
-        # 'command': 'button text'
-
+        'quiz': 'Поучаствовать в квизе ❓',
+        'translator': 'Перевести текст 📖',
+        'resume': 'Помощь с резюме 🧑‍🎓'
     })
 
 
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.callback_query.answer()
+    await send_text(update, context, 'Начнем сначала.')
+    await start(update, context)
+
+
+async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data['mode'] = 'random'
+    answer = await chat_gpt.send_question(load_prompt('random'), '')
+    await send_text_buttons(update, context, answer, buttons={
+        'random_more': 'Еще факт',
+        'stop': 'Завершить'
+    })
+
+
+async def random_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.callback_query.answer()
+    await random(update, context)
+
+
+async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data['mode'] = 'gpt'
+    chat_gpt.set_prompt(load_prompt('gpt'))
+    text = load_message('gpt')
+    await send_image(update, context, 'gpt')
+    await send_text(update, context, text)
+
+
+async def gpt_dialog(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    request = update.message.text
+    message = await send_text(update, context, 'Думаю над ответом...')
+    answer = await chat_gpt.add_message(request)
+    await message.delete()
+    await message.send_text_buttons(update, context,
+                                    answer,
+                                    buttons={'stop': 'Завершить'})
+
+
+async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.user_data['mode'] in ('main', 'random'):
+        await start(update, context)
+    elif context.user_data['mode'] == 'gpt':
+        await gpt_dialog(update, context)
+
+
 chat_gpt = ChatGptService('ChatGPT TOKEN')
-app = ApplicationBuilder().token('Telegram TOKEN').build()
+app = Application.builder().token(token).build()
 
-# Зарегистрировать обработчик команды можно так:
-# app.add_handler(CommandHandler('command', handler_func))
 
-# Зарегистрировать обработчик коллбэка можно так:
+app.add_handler(MessageHandler(filters.TEXT & filters.COMMAND, text_handler))
+app.add_handler(CommandHandler('start', start))
+app.add_handler(CommandHandler('random', random))
+app.add_handler(CommandHandler('gpt', gpt))
+#app.add_handler(CommandHandler('talk', talk))
+#app.add_handler(CommandHandler('quiz', quiz))
+#app.add_handler(CommandHandler('translator', translator))
+#app.add_handler(CommandHandler('resume', resume))
+
+
 # app.add_handler(CallbackQueryHandler(app_button, pattern='^app_.*'))
+app.add_handler(CallbackQueryHandler(random_buttons, pattern='random_more'))
+app.add_handler(CallbackQueryHandler(stop, pattern='stop'))
 app.add_handler(CallbackQueryHandler(default_callback_handler))
 app.run_polling()
